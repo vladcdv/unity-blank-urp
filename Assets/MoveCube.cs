f0ba0d1803da74a29054b4d61242c3a855fe2ed8
vladcdv <upekar@edu.cdv.p>
2023-03-23 16:26:37 +0100

    2 players, movement, input

---
+++ Assets/MoveCube.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rigidbody2D;
+
+    public float speed = 1;
+    public bool otherPlayer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (otherPlayer)
+        {
+            spriteRenderer.color = Color.red;
+        } else
+        {
+            spriteRenderer.color = Color.blue;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(otherPlayer ? KeyCode.UpArrow : KeyCode.W))
+        {
+            rigidbody2D.velocity = speed * Vector2.up;
+
+        } else if (Input.GetKey(otherPlayer ? KeyCode.DownArrow : KeyCode.S))
+        {
+            rigidbody2D.velocity = speed * Vector2.down;
+        } else
+        {
+            rigidbody2D.velocity = Vector2.zero;
+        }
+    }
+}
